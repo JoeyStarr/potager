9513e5b1b36bd27a634e775b80a86455fd7c41a6
JoeyStarr <tondossamakader@gmail.com>
2022-12-27 18:26:27 +0100

    fin page partage & debut modal commande

--- component/partageScreen.js
+++ component/partageScreen.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import styles from '../style';
 import Ionic from 'react-native-vector-icons/Ionicons'
-import { Text, View, Pressable , Image} from 'react-native';
+import { Text, View, Pressable , Image, TextInput, Modal} from 'react-native';
 
-const Partage = () => {
+const Partage = ({navigation}) => {
 
-  const onPressFunction1 = () => {
-    return
-  }
+  const [produit,setProduit] = useState("")
+  const [quantite,setQuantite] = useState()
+  const [prix,setPrix] = useState()
+  const [modalVisible, setModalVisible] = useState(false);
+
   const onPressFunction2 = () => {
     return
   }
@@ -15,13 +17,62 @@ const Partage = () => {
   return (
     <View style={styles.container2}>
       <View style={styles.header}>
-        <Pressable onPress={onPressFunction1}>
+        <Pressable onPress={() => {navigation.navigate('Accueil')}}>
           <Ionic name='arrow-back-outline' size='28' colour='black' />
         </Pressable>
-        <Text>Publier une offre d'achat</Text>
-        <Pressable onPress={onPressFunction2}>
+        <Text style={{fontSize:22}}>Publier une offre d'achat</Text>
+        <Pressable onPress={() => setModalVisible(true)}>
           <Image source={require('../assets/box.png')} style={{width:24,height:24}}/>
         </Pressable>
+      </View>
+      <View style={styles.body}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>Fermez</Text>
+            </Pressable>
+            <Text style={styles.modalText}>Hello World!</Text>
+          </View>
+        </View>
+      </Modal>
+          <TextInput
+            style={styles.input}
+            placeholder="Produit"
+            placeholderTextColor="#FFFFFF"
+            onChangeText={setProduit}
+            value={produit}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Quantité"
+            placeholderTextColor="#FFFFFF"
+            onChangeText={setQuantite}
+            keyboardType="numeric"
+            value={quantite}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Prix"
+            placeholderTextColor="#FFFFFF"
+            onChangeText={setPrix}
+            keyboardType="numeric"
+            value={prix}
+          />
+          <Pressable onPress={onPressFunction2} style={styles.pressbutt}>
+            <Text style={{fontSize:16,color:'white'}}>Valider</Text>
+          </Pressable>
       </View>
     </View>
   );
--- style.js
+++ style.js
@@ -1,5 +1,8 @@
 import { StyleSheet, Dimensions } from 'react-native';
 
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
 const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -8,11 +11,78 @@ const styles = StyleSheet.create({
       justifyContent: 'center',
     },
     container2: {
-        flex: 1,
-        backgroundColor: '#fff',
+        flex:1,
+        height:windowHeight,
+        width: windowWidth,
+        backgroundColor: '#00ABB3',
         alignItems: 'center',
         justifyContent: 'center',
       },
+    header:{
+        flex:1,
+        height: windowHeight - 100,
+        alignItems:'center',
+        flexDirection:'row',
+        justifyContent:'space-around',
+        width: '100%',
+        backgroundColor: '#F5F5F5',
+    },
+    body:{
+        flex:5,
+        alignItems: 'center',
+        width: windowWidth,
+        backgroundColor: '#F5F5F5',
+    },
+    input: {
+        height: 50,
+        width:'80%',
+        margin: 12,
+        marginVertical:20,
+        borderWidth: 1,
+        borderColor:'#CDC9C3',
+        backgroundColor:'#CDC9C3',
+        color:'grey',
+        borderRadius:8,
+        padding: 10,
+    },
+    pressbutt:{
+        height: 50,
+        borderRadius:8,
+        marginTop:20,
+        width:'50%',
+        padding: 12,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor:'#212529'
+    },
+    modalView: {
+      margin: 40,
+      backgroundColor: "white",
+      borderRadius: 20,
+      padding: 35,
+      alignItems: "center",
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 4,
+      elevation: 5
+    },
+    button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2
+    },
+    buttonClose: {
+        backgroundColor: "#000000",
+    },
+    textStyle: {
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center"
+    },
   });
   
   export default styles;
