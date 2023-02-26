823347171d9f17cbf62eb2bc7c66e50926326f43
JoeyStarr <tondossamakader@gmail.com>
2023-02-26 17:12:13 +0000

    options commande

--- screens/AdminPage/UsersCommand.js
+++ screens/AdminPage/UsersCommand.js
@@ -17,12 +17,14 @@ import {
   StatusBar,
   Pressable,
   Image,
+  Modal,
   TextInput,
   Dimensions,
   TouchableOpacity,
   Alert,
   ScrollView,
   ActivityIndicator,
+  Button,
 } from "react-native";
 
 // STYLES
@@ -30,6 +32,8 @@ import { FONTS, SIZES } from "../../style/theme";
 
 // Toast
 import { useToast } from "react-native-toast-notifications";
+
+import Slider from "@react-native-community/slider";
 
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
@@ -39,24 +43,66 @@ import { getAllAdvices, deleteAdvice } from "../../firebase/adviceCalls";
 
 const Command = ({ navigation }) => {
   const [advices, setAdvices] = useState(null);
+  const [donne, setDonne] = useState(null);
   const [search, setSearch] = useState("");
   const [data, setData] = useState([{}]);
   const [dataProducts, setDataProducts] = React.useState(null);
   // Search state
   const [isSearching, setIsSearching] = React.useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  
+  const [modalVisible, setModalVisible] = useState(false);
 
+
+  //filter commande
+
+  const [nm,setNm] = useState(null)
+
+  //
+
+  const [range,setRange] = useState('50%')
+  const [sliding,setSliding] = useState('Inactive')
+
+  const [taille,setTaille] = useState(null)
   const toast = useToast();
 
 
   const getAllAdvices = async () => {
     const advices = collection(db, "potager");
+    const donnees = collection(db, "command");
+
+    const q = query(advices);
+    const querySnapshot = await getDocs(q);
+
+    const p = query(donnees);
+    const querySnapshot1 = await getDocs(p);
+  
+    let datas = [];
+    querySnapshot.forEach((doc) => {
+      let num = 0;
+      querySnapshot1.forEach((dc) => {
+        if(dc.data().idSeller === doc.data().owner){
+          num++
+        }
+        console.log(dc.data().idSeller," : ",doc.data().owner)
+      })
+      console.log(num)
+      const newData = { ...doc.data(), ref: doc.id, number:num };
+      datas.push(newData);
+    });
+  
+    return datas;
+  };
+
+  const getAllDonne = async () => {
+    const advices = collection(db, "command");
   
     const q = query(advices);
     const querySnapshot = await getDocs(q);
   
     let datas = [];
     querySnapshot.forEach((doc) => {
+      console.log(doc.data())
       const newData = { ...doc.data(), ref: doc.id };
       datas.push(newData);
     });
@@ -71,7 +117,7 @@ const Command = ({ navigation }) => {
     console.log(newArr)
     return newArr
   };
-
+  
   const onApply = () => {
     const dataFiltered = handleSearch(search, advices);
     setData(null);
@@ -105,10 +151,45 @@ const Command = ({ navigation }) => {
     if (dataProducts == null) {
       getAllAdvices().then((data) => {
         setAdvices(data);
+        setTaille(data.length)
       });
+      getAllDonne().then((data) => {
+        setDonne(data)
+      })
     }
   },[]);
 
+  const slicer = (sli) => {
+    getAllAdvices().then((data) => {
+      let dada = data.slice(0,sli)
+      setAdvices(dada);
+    });
+  }
+
+  const option = () => {
+    getAllAdvices().then((data) => {
+      setAdvices(data);
+      setTaille(data.length)
+    });
+    setRange(taille)
+    setModalVisible(true)
+  }
+
+  /*
+  useEffect(() => {
+    console.log("okk",advices)
+    let newtab = advices.forEach((product) => {
+      if(product.number === nm){
+        return product.data()
+      }
+    })
+    if(newtab.length !== null){
+      setAdvices(newtab)
+    }
+  },[nm])
+  */
+
+  console.log(nm)
   return (
     <View style={styles.container}>
       <View style={styles.head}>
@@ -131,16 +212,71 @@ const Command = ({ navigation }) => {
             value={search}
           />
           <Pressable
+          /*
             onPress={() => {
               if (search?.length > 0) {
                 setFilterVisible(true);
               }
             }}
+            */
+            onPress={option}
           >
             <Ionic name="options-outline" size="22" colour="black" />
           </Pressable>
         </View>
       </View>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Text style={styles.textStyle}>Fermer</Text>
+            </Pressable>
+            <Text style={styles.modalText}>Filter</Text>
+            <Text>{range}</Text>
+            <View style={{flexDirection:'row',alignItems:'center',justifyContent:'space-between'}}>
+              <Text>1</Text>
+              <Slider
+                style={{width:250,height:40}}
+                minimumValue={1}
+                maximumValue={taille}
+                minimumTrackTintColor="tomato"
+                maximumTrackTintColor="#000"
+                thumbTintColor="tomato"
+                value={taille}
+                step={1}
+                onValueChange={value => setRange(parseInt(value))}
+                onSlidingStart={() => setSliding('Sliding')}
+                onSlidingComplete={(val) => slicer(val)} 
+              />
+              <Text>{taille}</Text>
+            </View>
+            <Text style={styles.modalText}>Filter par nombre de commande</Text>
+            <View style={{flexDirection:'row',alignItems:'center',justifyContent:'space-between'}}>
+            <Pressable  style={styles.bull} onPress={() => setNm(10)}>
+              <Text>10</Text>
+            </Pressable>
+            <Pressable style={styles.bull} onPress={() => setNm(20)}>
+              <Text>20</Text>
+            </Pressable>
+            <Pressable style={styles.bull} onPress={() => setNm(30)}>
+              <Text>30</Text>
+            </Pressable>
+            <Pressable style={styles.bull} onPress={() => setNm(50)}>
+              <Text>50+</Text>
+            </Pressable>
+            </View>
+          </View>
+        </View>
+      </Modal>
       <ScrollView style={styles.body}>
         <View
           style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
@@ -223,6 +359,9 @@ const renderAdvice = ({ advice, navigation }) => {
         })}
       >
         <Ionic name="basket-outline" size="25" color="white" />
+        <View style={styles.cartBubble}>
+            <Text style={{ color: "black" }}>{advice.number}</Text>
+        </View>
       </TouchableOpacity>
     </View>
   );
@@ -329,5 +468,71 @@ const styles = StyleSheet.create({
     color: "grey",
     borderRadius: 8,
     padding: 10,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent:'flex-end',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    marginTop: 20,
+    width:'100%',
+    height:'60%',
+    bottom:0,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: 'black',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    marginTop: 15,
+    fontSize:22,
+    textAlign: 'center',
+  },
+  cartBubble: {
+    borderWidth: 1,
+    width: 20,
+    height: 20,
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+
+    position: "absolute",
+    top: -10,
+    right: -7,
+    backgroundColor: "white",
+  },
+  bull:{
+    borderWidth:1,
+    borderRadius:10,
+    padding:20,
+    marginHorizontal:5,
+    borderColor:'black'
   }
 });
